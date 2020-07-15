5b81c473f3d80da3eea422927e944786d05d66b5
KilFront <kaifawebb@gmail.com>
2020-07-15 11:21:12 +0800

    express

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ### breach
 #### module-system
 Node.js模块系统
+
+#### express
+how to use express.js
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo 'git auto push start...'
 git add .
-git commit -m 'update'
-git push origin master
+git commit -m ${2:-update}
+git push origin ${1}
 echo 'git auto push end!'
